910fa1310baa307350c02ca471bd8093d1f40d83
pavloSpaceship <pavlo.vishnyak@gmail.com>
2023-12-11 15:19:56 +0200

    added homework 3 with node practise

---
+++ src/Homework3.js
@@ -0,0 +1,20 @@
+name = "Pavlo";
+age = 25;
+IsGenderMan = true;
+divideon0 = 1/0;
+BigInt = 12345n;
+symbol = Symbol();
+obj = {
+    name : "Pavlo",
+    age : 25,
+}
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof IsGenderMan);
+console.log(typeof variableisnotexist);
+console.log(typeof name / 1);
+console.log(Infinity + divideon0);
+console.log(typeof BigInt);
+console.log(typeof symbol);
+console.log(typeof obj);
